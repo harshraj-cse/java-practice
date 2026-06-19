7c46c3a39bd835432ad12c38db6faa7dab33689f
harshraj-cse <harshr8252@gmail.com>
2026-06-19 12:33:38 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostresm>
+using namespace std;
+int main({
+    int arr[]={5,6,2,9,1};
+    int sum=0;
+    for (int i = 0; i <5; i++){
+        sum=sum+arr[i];
+    }
+    cout <<"sum of array element ="<<sum;
+    return 0;
+})
